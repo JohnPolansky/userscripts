844d9389428c19a24ac9d222f46d12e95b6b4640
JohnPolansky <JohnPolansky@users.noreply.github.com>
2022-02-15 13:03:17 -0600

    Update AWS-SSO-Console-User-Title-Fix.js
    Fixes for latest AWS Console changes

--- AWS-SSO-Console-User-Title-Fix.js
+++ AWS-SSO-Console-User-Title-Fix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWS-SSO-Console-User-Title-Fix
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  With AWS SSO the AWS Console Title is ridiculously long, this aims to shorten and list the account name
 // @author       John Polansky
 // @icon         https://www.google.com/s2/favicons?domain=userscript.zone
@@ -9,7 +9,6 @@
 // @include      https://*.console.aws.amazon.*
 // @run-at       document-end
 // @inject-into content
-// @version      0.0.1
 // @license      MIT
 // @noframes
 // @grant window.close
@@ -18,12 +17,18 @@
 
 (function() {
     'use strict';
+    // console.log("======================================================")
     // The ClassName might change with updates, find the title in inspect and replace it here
-    let str = document.getElementsByClassName("ThRjn7o-KwO0459UzmvoU")[0].title
+    // let str = document.getElementsByClassName("ThRjn7o-KwO0459UzmvoU")[0].title
+    let str = document.getElementsByClassName("_1Vtx1Z7gxtNZJP2MVzVCLO")[2].title
+    // console.log(`Element: ${str})
+    // AWSReservedSSO_AWSAdministratorAccess_ca3eaf088602997d/john.polansky@itential.com @ itential-cloudops
     let re = /\w+_(\w+)_(\w+)\/(.*)\s+@\s+(.+)/
     let results = re.exec(str)
     let role = results[1]
     let username = results[3].replace(/@.*$/, "")
     let account = results[4]
-    document.getElementsByClassName("ThRjn7o-KwO0459UzmvoU")[0].innerHTML = `${account} / ${role} / ${username}`
+    // console.log(`Username: ${username}  Account: ${account}  Role: ${role}`)
+    // console.log("======================================================")
+    document.getElementsByClassName("_1Vtx1Z7gxtNZJP2MVzVCLO")[2].innerHTML = `${account} / ${role} / ${username}`
 })();
